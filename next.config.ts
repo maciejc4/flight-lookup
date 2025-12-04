379de99c2cc48a353299e42050166af1ed86f237
Maciej Jasinski <maciejc4@gmail.com>
2025-12-04 11:37:56 +0100

    Revert "Add GitHub Pages deployment workflow"
    This reverts commit 7c80689169f05da05a52900ea34f7a3a11998d61.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/flight-lookup",
-  images: { unoptimized: true },
+  /* config options here */
 };
 
 export default nextConfig;
